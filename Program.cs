c107882bc64f906135660c366fbed731941a94ff
Jonathan Schaffer <jonizzyschaffer@gmail.com>
2019-05-24 13:59:43 -0500

    solved the problem

--- Program.cs
+++ Program.cs
@@ -14,13 +14,17 @@ namespace CSharpIntroPlayTime
             // Get text the user types into the console
             string response = Console.ReadLine();
 
-            if (response == "A" || response == "a")
+            if (response == "A")
             {
                 GroceryList();
             }
-            else  // change this to only call GuessingGame() when the user enters option "B"
+            else if(response == "B") // change this to only call GuessingGame() when the user enters option "B"
             {
                 GuessingGame();
+            }
+            else  {
+                Console.WriteLine("Invalid Option!");
+                Main(null);
             }
 
             // If the user didn't input an "A" or "B" print a message 
@@ -40,6 +44,9 @@ namespace CSharpIntroPlayTime
                 groceries.Add(input);
 
                 // Ask for the input again
+                Console.WriteLine("Enter some groceries. (Enter a blank line when finished)");
+                input = Console.ReadLine();
+
             }
 
             Console.WriteLine();
@@ -47,6 +54,7 @@ namespace CSharpIntroPlayTime
             foreach (string grocery in groceries)
             {
                 // Write the grocery to the console
+                Console.WriteLine(grocery);
             }
         }
 
@@ -54,22 +62,30 @@ namespace CSharpIntroPlayTime
         {
             // Get a random number between 1 and 20
             //  This is the number the user is trying to guess
-            int answer = new Random().Next(1, 21);
+            int answer = new Random().Next(1, 20);
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 Console.Write("Guess a number between 1 and 20: ");
                 int guess = int.Parse(Console.ReadLine());
 
                 // Add a conditional to determine if the user's guess is higher than the answer
                 //  If so, print "Too High!" to the console
+                if (guess >= answer){
+                    Console.WriteLine("Too High");
+                }
+                else if( guess <= answer) {
+                    Console.WriteLine("Too low");
+                } else {
+                        Console.WriteLine("You got it!");
+                return;
+                }
+
 
                 // Add a conditional to determine if the user's guess is lower than the answer
                 //  If so, print "Too Low!" to the console
 
                 // When the user guesses correctly, tell them and return from the method
-                Console.WriteLine("You got it!");
-                return;
             }
 
             Console.WriteLine("Better luck next time...");
